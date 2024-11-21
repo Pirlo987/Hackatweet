bf12da2a3d9c5e420106e109d0f414f1e175af7d
Pierre Roitg <pierre.roitg@live.fr>
2024-11-21 17:06:50 +0100

    route tweet hasht

--- backend/models/tweet.js
+++ backend/models/tweet.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const tweetSchema = mongoose.Schema({
   user: { type: mongoose.Schema.Types.ObjectId, ref: "users" },
   tweet: String,
-  hashtag: String,
+  hashtag: [String],
   creationDate: { type: Date, default: Date.now },
   like: [{ type: mongoose.Schema.Types.ObjectId, ref: "users" }],
 });
