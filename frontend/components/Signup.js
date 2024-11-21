ebdb8cb2f86605bbc5e4e9597af17eeefe4b5040
Carla <carlamoulie@gmail.com>
2024-11-21 12:20:57 +0100

    modal

---
+++ frontend/components/Signup.js
@@ -0,0 +1,42 @@
+import { Button, Modal } from 'antd';
+import styles from '../styles/Signup.module.css';
+import React, { useState } from 'react';
+
+
+function Signup(){
+
+      const [isModalOpen, setIsModalOpen] = useState(false);
+      const showModal = () => {
+        setIsModalOpen(true);
+      };
+      const handleOk = () => {
+        setIsModalOpen(false);
+      };
+      const handleCancel = () => {
+        setIsModalOpen(false);
+      };
+      return (
+        <>
+          <Button type="primary" onClick={showModal}>
+            Signup
+          </Button>
+          <Modal open={isModalOpen} onOk={handleOk} onCancel={handleCancel} className={styles.modalSignup}>
+          <div className={styles.signupContainer}>
+                <img src='logo_twitter.png'/>
+                <h1>Create your Hackatweet account</h1>
+                <input className={styles.inputSignup} type='text'></input>
+                <input className={styles.inputSignup} type='text'></input>
+                <input className={styles.inputSignup} type='text'></input>
+                <button>Signin</button>
+            </div>
+            
+          </Modal>
+        </>
+      );
+    }
+
+
+
+
+
+export default Signup
